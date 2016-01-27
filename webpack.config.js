c3dd89f976637c79b71fb7a080f11b2cf18d2e94
Johan Dornell <johan@thebrewery.se>
2016-01-27 12:28:05 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    entry: "./src/js/app.js", //name of file(s) that should be included in the build
+    output: {
+        filename: "./tools/bundle.js" //the name of the file that Webpack should create
+    }
+}
