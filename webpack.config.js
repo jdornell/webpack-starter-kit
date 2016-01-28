ecc156527b652851894f274e82f0c62a1130c814
Johan Dornell <johan@thebrewery.se>
2016-01-28 12:20:36 +0100

    refinements

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    entry: "./src/js/app.js", //name of file(s) that should be included in the build
-    output: {
-        filename: "./tools/bundle.js" //the name of the file that Webpack should create
-    }
+  entry: "./app.js",
+  output: {
+    filename: "bundle.js"
+},
+watch: true
 }
