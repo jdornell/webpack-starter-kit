1376fd1a49dfced7236bc26f663480e89ac740be
Johan Dornell <johan@thebrewery.se>
2016-01-28 12:39:22 +0100

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,18 @@ module.exports = {
   output: {
     filename: "bundle.js"
 },
+module: {
+   loaders: [
+      {
+         test: /\.js$/,
+         exclude: /node_modules/,
+         loader: 'babel-loader',
+         query: {
+            cacheDirectory: true,
+            presets: ['react', 'es2015']
+         }
+      }
+   ]
+},
 watch: true
 }
